1ebb9431723fe095314bcd6820d830aee874f587
oansky <oana.anghelachi@gmail.com>
2022-01-03 16:21:03 +0100

    Implement button functionality and call the fetch API

--- app.js
+++ app.js
@@ -11,3 +11,19 @@ let dog = document.querySelector(".dog")
 const catButton = document.querySelector(".get-cat")
 const foxButton = document.querySelector(".get-fox")
 const dogButton = document.querySelector(".get-dog")
+
+// Add eventListeners on BTNs
+catButton.addEventListener("click", getCat)
+foxButton.addEventListener("click", getCat)
+dogButton.addEventListener("click", getCat)
+
+//
+function getCat() {
+	// the fetch(API) is going to return a PROMISE
+	fetch("https://aws.random.cat/meow")
+		// the .json() method is going to do 3 things: READ, Convert to JSON file, PARSE that JSON file
+		.then(response => response.json())
+		.then(responseData => {
+			cat.innerHTML = `<img src="${responseData.file}" />`
+		})
+}
