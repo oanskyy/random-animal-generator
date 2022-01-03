7ddc30932b25402f7e79de60288e7b1fbb101aa3
oansky <oana.anghelachi@gmail.com>
2022-01-03 15:51:05 +0100

    Grab all the DOM selectors we will use

--- app.js
+++ app.js
@@ -3,4 +3,11 @@ Public API Repo
 https://github.com/public-apis/public-apis
 */
 
+// Grab all the elements we need and store them in variables
+let cat = document.querySelector(".cat")
+let fox = document.querySelector(".fox")
+let dog = document.querySelector(".dog")
 
+const catButton = document.querySelector(".get-cat")
+const foxButton = document.querySelector(".get-fox")
+const dogButton = document.querySelector(".get-dog")
